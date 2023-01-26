c4f24b16b63512b5aa5fa8e0e72bf982de670000
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-01-26 13:41:04 +0700

    Add documentation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 /**
- * @param {string} word 
+ * Converts the specified word to Title Case.
+ * @param {string} word
+ * @returns {string} word in Title Case.
  */
 function wordTitleCase(word) {
     return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
+/**
+ * Randomly returns "rock"/"paper"/"scissors".
+ * @returns {string} "rock"/"paper"/"scissors".
+ */
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
 
@@ -19,34 +25,39 @@ function getComputerChoice() {
 }
 
 /**
- * @param {string} playerSelection
- * @param {string} computerSelection
+ * Plays one round of rock paper scissors and determines the winner.
+ * @param {string} playerSelection - "rock"/"paper"/"scissors" (case insensitive).
+ * @param {string} computerSelection - "rock"/"paper"/"scissors" (case insensitive).
+ * @returns {string} message that inform the result.
  */
 function playRound(playerSelection, computerSelection) {
-    const playerSelectionLowerCase = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-    // Check playerSelection
-    if (playerSelectionLowerCase !== "rock" && playerSelectionLowerCase !== "paper" && playerSelectionLowerCase !== "scissors") {
+    // Check playerSelection validity
+    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
         return "Player selection invalid!"
     }
 
-    // Create adjacency list for Rock Paper Scissors graph
-    // Rock Paper Scissors graph is directed graph
-    // Edge (x, y) means x beats y
+    // Create adjacency list for Rock Paper Scissors directed graph.
+    // Edge (x, y) means x beats y.
     const adjList = new Map();
     adjList.set("rock", "scissors");
     adjList.set("scissors", "paper");
     adjList.set("paper", "rock");
 
-    if (playerSelectionLowerCase === computerSelection) {
+    if (playerSelection === computerSelection) {
         return `Draw! ${wordTitleCase(playerSelection)} draws with ${wordTitleCase(computerSelection)}`;
-    } else if (adjList.get(playerSelectionLowerCase) === computerSelection) {
+    } else if (adjList.get(playerSelection) === computerSelection) {
         return `You Win! ${wordTitleCase(playerSelection)} beats ${wordTitleCase(computerSelection)}`;
     } else {
         return `You Lose! ${wordTitleCase(playerSelection)} is defeated by ${wordTitleCase(computerSelection)}`
     }
 }
 
+/**
+ * Starts the rock paper scissors game.
+ */
 function game() {
     let playerScore = 0;
     let computerScore = 0;
@@ -54,6 +65,7 @@ function game() {
     let totalInvalid = 0;
     const totalRound = 5;
 
+    // Play round
     for (let i = 1; i <= totalRound; i++) {
         const playerSelection = prompt(`Round ${i}: Enter your selection (Rock/Paper/Scissors) `) ?? "";
         const computerSelection = getComputerChoice();
@@ -69,12 +81,11 @@ function game() {
         } else {
             totalInvalid++;
         }
-
     }
 
     const playerStatus = `(Your status = W: ${playerScore}; L: ${computerScore}; D: ${totalDraw}; I: ${totalInvalid})`;
 
-    // Report Winner
+    // Report winner
     if (playerScore > computerScore) {
         console.log(`Player is the Winner ${playerStatus}`);
     } else if (playerScore < computerScore) {
@@ -84,5 +95,4 @@ function game() {
     }
 }
 
-// Start game
 game();
