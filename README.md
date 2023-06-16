830f0ee45498e5a650f02307749c5b0e6dfcfe16
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-06-16 19:03:02 +0700

    Implement the game logic

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ List of resources that helped me to create this project:
 - [How to use flex to shrink an image](https://www.geeksforgeeks.org/how-to-use-flex-to-shrink-an-image-in-css/)
 - [How to create a vertical line](https://www.w3schools.com/howto/howto_css_vertical_line.asp)
 - [\<input type="image"\>](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/image)
+- [How to remove all event listeners from a DOM element in Javascript](https://techoverflow.net/2019/12/26/how-to-remove-all-event-listeners-from-a-dom-element-in-javascript/)
+- [How to darken an Image using CSS ?](https://www.geeksforgeeks.org/how-to-darken-an-image-using-css/)
+- [Sleep in JavaScript](https://www.sitepoint.com/delay-sleep-pause-wait/)
--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
 /**
- * Converts the specified word to Title Case.
- * @param {string} word
- * @returns {string} word in Title Case.
- */
-function wordTitleCase(word) {
-    return word[0].toUpperCase() + word.slice(1).toLowerCase();
-}
-
-/**
  * Randomly returns "rock"/"paper"/"scissors".
  * @returns {string} "rock"/"paper"/"scissors".
  */
@@ -28,7 +19,7 @@ function getComputerChoice() {
  * Plays one round of rock paper scissors and determines the winner.
  * @param {string} playerSelection - "rock"/"paper"/"scissors" (case insensitive).
  * @param {string} computerSelection - "rock"/"paper"/"scissors" (case insensitive).
- * @returns {string} message that inform the result.
+ * @returns {string} player status ("DRAW"/"WIN"/"LOSE").
  */
 function playRound(playerSelection, computerSelection) {
     const playerSelectionLower = playerSelection.toLowerCase();
@@ -47,11 +38,11 @@ function playRound(playerSelection, computerSelection) {
     ]);
 
     if (playerSelectionLower === computerSelectionLower) {
-        return `Draw! ${wordTitleCase(playerSelectionLower)} draws with ${wordTitleCase(computerSelectionLower)}`;
+        return "DRAW";
     } else if (adjList.get(playerSelectionLower) === computerSelectionLower) {
-        return `You Win! ${wordTitleCase(playerSelectionLower)} beats ${wordTitleCase(computerSelectionLower)}`;
+        return "WIN";
     } else {
-        return `You Lose! ${wordTitleCase(playerSelectionLower)} is defeated by ${wordTitleCase(computerSelectionLower)}`
+        return "LOSE";
     }
 }
 
@@ -59,44 +50,239 @@ function playerSelectionIsValid(playerSelection) {
     return playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors";
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let totalDraw = 0;
+let currentRound = 0;
+const scoreToWin = 5;
+
 /**
  * Starts the rock paper scissors game.
  */
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let totalDraw = 0;
-    let totalInvalid = 0;
-    const totalRound = 5;
+    enableAllInput();
+    addInfoBoard();
+    addResetButton();
+    addAllSelectionsListener();
+}
 
-    // Play round
-    for (let i = 1; i <= totalRound; i++) {
-        const playerSelection = prompt(`Round ${i}: Enter your selection (Rock/Paper/Scissors) `) ?? "";
-        const computerSelection = getComputerChoice();
-        const result = `Round ${i}: ${playRound(playerSelection, computerSelection)}`;
-        console.log(result);
+function addAllSelectionsListener() {
+    const selections = document.querySelectorAll("#player input");
+    selections.forEach((selection) => {
+        selection.addEventListener("click", () => {
+            const playerSelection = selection.dataset.key;
+            setElementBrightness(selection, 100);
 
-        if (result.includes("Win")) {
+            const computerSelection = getComputerChoice();
+            const computerSelectionInput = document.querySelector(`#computer input[data-key="${computerSelection}"]`);
+            setElementBrightness(computerSelectionInput, 100);
+
+            const result = playRound(playerSelection, computerSelection)
+            updateScore(result);
+            updateInfo();
+
+            disableAllInput();
+            setTimeout(() => {
+                resetInputBrightness();
+                if (existWinner()) {
+                    reportWinner();
+                    resetScore();
+                    addPlayAgainAndBackHomeButton();
+                } else {
+                    enableAllInput();
+                }
+            }, 1000);
+
+        })
+    });
+}
+
+function setElementBrightness(elem, percent) {
+    elem.style.filter = `brightness(${percent}%)`;
+}
+
+function existWinner() {
+    return playerScore == scoreToWin || computerScore == scoreToWin;
+}
+
+// UPDATE
+function updateScore(result) {
+    switch (result) {
+        case "WIN":
             playerScore++;
-        } else if (result.includes("Lose")) {
+            break;
+        case "LOSE":
             computerScore++;
-        } else if (result.includes("Draw")) {
+            break;
+        case "DRAW":
             totalDraw++;
-        } else {
-            totalInvalid++;
-        }
-    }
-
-    const playerStatus = `(Your status = W: ${playerScore}; L: ${computerScore}; D: ${totalDraw}; I: ${totalInvalid})`;
-
-    // Report winner
-    if (playerScore > computerScore) {
-        console.log(`Player is the Winner ${playerStatus}`);
-    } else if (playerScore < computerScore) {
-        console.log(`Computer is the Winner ${playerStatus}`);
-    } else {
-        console.log(`Match ended with Draw ${playerStatus}`);
+            break;
     }
 }
 
-// game();
+function updateInfo() {
+    updateRound(currentRound++)
+    updateScoreBoard()
+}
+
+function updateRound(round) {
+    const roundDiv = document.getElementById("round-number");
+    roundDiv.textContent = round;
+}
+
+function updateScoreBoard() {
+    const playerScoreDiv = document.getElementById("player-score");
+    const computerScoreDiv = document.getElementById("computer-score");
+
+    playerScoreDiv.textContent = String(playerScore);
+    computerScoreDiv.textContent = String(computerScore);
+}
+
+// RESET
+function resetGame() {
+    resetScore();
+    resetInfoBoard();
+    resetInputBrightness();
+    removeAllSelectionsListener();
+    addPlayButton();
+}
+
+function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+    totalDraw = 0;
+    currentRound = 0;
+}
+
+function resetInfoBoard() {
+    const infoDiv = document.getElementById("info");
+    infoDiv.textContent = "First to 5 win the game";
+}
+
+function removeAllSelectionsListener() {
+    const selections = document.querySelectorAll("#player input");
+
+    selections.forEach((selection) => {
+        selection.replaceWith(selection.cloneNode(true));
+    });
+}
+
+// INPUT
+function resetInputBrightness() {
+    const selections = document.querySelectorAll("input");
+
+    selections.forEach((selection) => {
+        selection.style.removeProperty("filter");
+    })
+}
+
+function disableAllInput() {
+    const selections = document.querySelectorAll("input");
+
+    selections.forEach((selection) => {
+        selection.disabled = true;
+    })
+}
+
+function enableAllInput() {
+    const selections = document.querySelectorAll("input");
+
+    selections.forEach((selection) => {
+        selection.disabled = false;
+    })
+}
+
+// REPORT
+function reportWinner() {
+    const playerStatus = `(Your status = Win: ${playerScore}; Lose: ${computerScore}; Draw: ${totalDraw})`;
+
+    let result;
+
+    if (playerScore > computerScore) {
+        result = "You are the Winner";
+    } else if (playerScore < computerScore) {
+        result = "Computer is the Winner";
+    } else {
+        result = "Match ended with Draw";
+    }
+
+    const infoDiv = document.getElementById("info");
+    infoDiv.innerHTML = `
+        <p>${result}</p>
+        <p>${playerStatus}</p>
+    `;
+}
+
+// PLAY and RESET button
+function addResetButton() {
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset";
+    resetButton.classList.add("reset");
+    resetButton.addEventListener("click", resetGame);
+
+    const buttonContainer = document.getElementById("button");
+    buttonContainer?.replaceChildren(resetButton);
+}
+
+function addPlayButton() {
+    const playButton = document.createElement("button");
+    playButton.textContent = "Play";
+    playButton.classList.add("play");
+    playButton?.addEventListener("click", () => {
+        game();
+    });
+
+    const buttonContainer = document.getElementById("button");
+    buttonContainer?.replaceChildren(playButton);
+}
+
+function addPlayAgainAndBackHomeButton() {
+    const playAgainButton = document.createElement("button");
+    playAgainButton.textContent = "Play Again?";
+    playAgainButton.classList.add("play");
+    playAgainButton?.addEventListener("click", () => {
+        resetGame();
+        game();
+    });
+
+    const backHomeButton = document.createElement("button");
+    backHomeButton.textContent = "Back to home";
+    backHomeButton.classList.add("reset");
+    backHomeButton.addEventListener("click", resetGame);
+
+    const buttonContainer = document.getElementById("button");
+    buttonContainer?.replaceChildren(playAgainButton);
+    buttonContainer?.appendChild(backHomeButton);
+}
+
+function addInfoBoard() {
+    const info = `
+        <div id="round">
+                <div>Round</div>
+                <div id="round-number">0</div>
+            </div>
+            <div class="vertical-line"></div>
+            <div id="score">
+                <div>Score</div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>You</th>
+                            <th rowspan="2">Computer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td id="player-score">0</td>
+                            <td id="computer-score">0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    `;
+
+    const infoDiv = document.getElementById("info");
+    infoDiv.innerHTML = info;
+}
+
+addPlayButton();
